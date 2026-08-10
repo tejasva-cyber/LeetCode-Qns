c76835c7d8100cf3d5cc6ccfc2c31c02ddcca2c1
Tejasva tiwari <tejasvatiwari16@gmail.com>
2026-08-10 21:01:37 +0530

    Time: 23 ms (46.5%), Space: 16.4 MB (33.33%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        groups = {}
+
+        for s in strs:
+            key = ''.join(sorted(s))
+            groups.setdefault(key, []).append(s)
+
+        return list(groups.values())
