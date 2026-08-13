846c41de11c1e59cada1fe0406096cecf3398ba3
Tejasva tiwari <tejasvatiwari16@gmail.com>
2026-08-14 01:38:54 +0530

    Time: 7 ms (96.85%), Space: 16 MB (50.14%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,12 @@
+class Solution:
+    def merge(self, intervals):
+        intervals.sort()
+        result = []
+
+        for interval in intervals:
+            if not result or result[-1][1] < interval[0]:
+                result.append(interval)
+            else:
+                result[-1][1] = max(result[-1][1], interval[1])
+
+        return result
