4b6e9cdc708fb14dc9d95133dbd69e025c0ea0b3
Tejasva tiwari <tejasvatiwari16@gmail.com>
2026-08-22 23:01:41 +0530

    Time: 0 ms (100%), Space: 12.3 MB (91.6%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums):
+        if len(nums) == 1:
+            return nums[0]
+
+        def rob_line(arr):
+            prev2 = 0
+            prev1 = 0
+
+            for money in arr:
+                current = max(prev1, prev2 + money)
+                prev2 = prev1
+                prev1 = current
+
+            return prev1
+
+        return max(rob_line(nums[:-1]), rob_line(nums[1:]))
