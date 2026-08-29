cc1c11726183aa6423c531a6c89cecec5fd01edc
Tejasva tiwari <tejasvatiwari16@gmail.com>
2026-08-29 16:58:26 +0530

    Time: 3 ms (41.01%), Space: 12.2 MB (99.83%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numDecodings(self, s):
+        if not s or s[0] == "0":
+            return 0
+
+        prev2 = 1
+        prev1 = 1
+
+        for i in range(1, len(s)):
+            current = 0
+
+            if s[i] != "0":
+                current += prev1
+
+            if 10 <= int(s[i - 1:i + 1]) <= 26:
+                current += prev2
+
+            prev2 = prev1
+            prev1 = current
+
+        return prev1
