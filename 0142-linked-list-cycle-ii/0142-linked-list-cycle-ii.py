b0b61ab9a4f8023ff159cb0e05b1cc0eb8c1452a
Tejasva tiwari <tejasvatiwari16@gmail.com>
2026-09-11 23:18:40 +0530

    Time: 43 ms (5.19%), Space: 18.6 MB (90.09%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def detectCycle(self, head):
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                break
+        else:
+            return None
+
+        slow = head
+
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+
+        return slow
