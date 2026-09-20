9350046117e3f84cd3518223402fc734074848c9
Tejasva tiwari <tejasvatiwari16@gmail.com>
2026-09-21 00:06:59 +0530

    Time: 128 ms (58.39%), Space: 84.6 MB (6.25%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, head):
+        values = []
+
+        while head:
+            values.append(head.val)
+            head = head.next
+
+        return values == values[::-1]
